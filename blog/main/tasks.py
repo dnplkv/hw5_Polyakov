63cec8d375116c8b1913556159b9113d712c12a9
danny <dannypolyakov95@gmail.com>
2021-05-23 11:38:21 +0300

    pytest_br - added implementation for tests. Coverage moved from 40% up to 74%

--- blog/main/tasks.py
+++ blog/main/tasks.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
+from decimal import Decimal
 
 from celery import shared_task
 from django.utils.timezone import now
 import requests
 
-from .models import Log, Subscriber
+from .models import Log, Rate, Subscriber
 from .notify_service import email_send, email_send_string
 
 
@@ -26,3 +27,35 @@ def email_send_to_subs():
 
     for email in set(emails):
         email_send_string(email, string)
+
+
+@shared_task
+def parse_privatbank():
+    url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+    response = requests.get(url)
+
+    response.raise_for_status()
+
+    data = response.json()
+    source = 1
+    currency_map = {
+        'USD': 1,
+        'EUR': 2,
+    }
+
+    TWOPLACES = Decimal(10) ** -2
+
+    for row in data:
+        if row['ccy'] in currency_map:
+            buy = Decimal(row['buy']).quantize(TWOPLACES)
+            sale = Decimal(row['sale']).quantize(TWOPLACES)
+            currency = currency_map[row['ccy']]
+
+            last_rate = Rate.objects.filter(source=source, currency=currency).last()
+            if last_rate is None or buy != last_rate.buy or sale != last_rate.sale:
+                Rate.objects.create(
+                    currency=currency,
+                    source=source,
+                    buy=buy,
+                    sale=sale,
+                )

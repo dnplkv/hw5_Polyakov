e5001b86ddf41b4b710cb0387a3599806126ed75
danny <dannypolyakov95@gmail.com>
2021-06-04 15:09:04 +0300

    log_password_br - implemented auth.

--- blog/account/models.py
+++ blog/account/models.py
@@ -1,6 +1,9 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     email = models.EmailField('Email address', blank=False, null=False, unique=True)
+    confirmation_token = models.UUIDField(default=uuid.uuid4)
--- blog/account/urls.py
+++ blog/account/urls.py
@@ -1,9 +1,15 @@
+from account import views
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from . import views
 
 app_name = 'account'
 
 urlpatterns = [
     path('my-profile/', views.MyProfile.as_view(), name='my_profile'),
+    path('sign-up/', views.SignUpView.as_view(), name='sign_up'),
+    path('activate/<str:confirmation_token>', views.ActivateUserView.as_view(), name='activate'),
+    path('password/', views.change_password, name='change_password'),
+    path('logout/', views.logout, name='user_logout'),
+    path('login/', auth_views.LoginView.as_view(template_name="account/user_login.html"), name='user_login'),
 ]

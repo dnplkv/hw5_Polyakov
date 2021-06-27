d54e90fa841c3cc79f97530326553dac3026f2fd
danny <dannypolyakov95@gmail.com>
2021-06-27 11:41:46 +0300

    django_admin_br - created managers group, tested different admin functionality

--- blog/main/admin.py
+++ blog/main/admin.py
@@ -1,12 +1,37 @@
 from account.models import Ava, User
 from django.contrib import admin
 
-from .models import Author, Comment, Log, Post
+from .models import Author, Comment, Log, Post, Rate
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'created',
+    ]
+
+    list_filter = [
+        'title',
+        'created',
+    ]
+
+    readonly_fields = ['title', 'content']
+
+    def get_readonly_fields(self, request, obj=None):
+        request.user.groups.filter(name='managers')
+        if request.user.is_superuser:
+            return ()
+        return super().get_readonly_fields(request, obj)
+
+    # def has_delete_permission(self, request, obj=None):
+    #     return request.user.is_superuser
 
 
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Log)
 admin.site.register(User)
+
 admin.site.register(Ava)
+admin.site.register(Rate)

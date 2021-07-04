0388f211ee33a5ee80cf65f06a64ce80454c290d
danny <dannypolyakov95@gmail.com>
2021-07-04 12:33:37 +0300

    jquery_br - error with main.js (dont know why its now working)

---
+++ blog/main/static/js/main.js
@@ -0,0 +1,27 @@
+;(function() {
+    $(function() {
+        const $el = $('h1');
+        $el.append("<div><button class='show-new-text'>Show authors</button></div>");
+        const authorsUrl = "http://127.0.0.1/api/v1/authors/";
+
+        const $button = $el.find('.show-new-text');
+        const $page_container = $('#page-text');
+        const $imgEl = $('#page-text');
+
+        $button.click(function() {
+            $.get(authorsUrl, function(data, status){
+                $page_container.hide();
+                $page_container.empty();
+                $.each(data, function(index, value){
+                    $page_container.append(`<p>${index} - ${value.name}</p>`);
+                })
+                $page_container.fadeIn(5000);
+            })
+        })
+
+        $im.click(function(el) {
+            $(this)
+                .fadeOut(4000);
+        })
+    })
+})()
